1ef09da43766df07f2b2df9dc0b4e7973e55fb5e
NAT-G44 <gr.natasha.2003@gmail.com>
2024-10-23 22:41:15 +0800

    Add files via upload

---
+++ project00.py
@@ -0,0 +1,14 @@
+print (9**0.5*5) #1st program
+print (9.99 > 9.98 and 1000!=1000.1) #2nt program
+print (2*2+2) #3 rd program
+print (2*(2+2))
+print (2*2+2 == 2*(2+2))
+print (int(float("123.456")))
+print (int(float("123.456")*10))
+print (int(float("123.456")*10)%10)#4th program
+
+
+
+
+
+
